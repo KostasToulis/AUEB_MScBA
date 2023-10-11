a684787643eb1db5a551f007db65177f582290d4
KostasT5 <toulisk@gmail.com>
2023-10-11 13:04:35 +0300

    Minor fixes in nearest neigbor

--- tsp/generate_graph.py
+++ tsp/generate_graph.py
@@ -1,12 +1,14 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 class Node:
     def __init__(self, id, x , y):
         self.id = id
         self.x = x
         self.y = y
+        self.isVisited = False
 
 def CreateNodes(nodesNum):
     random.seed(1)
@@ -22,11 +24,23 @@ def PlotGraph(nodes):
 
     plt.show()
 
-# nodesNum = 20
-# # while (nodesNum <= 0 or nodesNum > 100):
-# #     nodesNum = int(input("Give value for number of nodes:"))
-#
-# nodes = CreateNodes(nodesNum)
-# coords = np.array([[i.x, i.y] for i in nodes])
-# plt.plot(coords[:,0], coords[:,1], linewidth = 0, marker='*', color='blue')
-# plt.show()
+def SelectStartNode(nodes):
+   random.seed(1)
+   start = random.randint(0,len(nodes)-1)
+   return nodes[start]
+
+def CalculateDistance(node1, node2):
+   return math.sqrt((node1.x - node2.x)**2 + (node1.y - node2.y)**2)
+
+def CalculateDistMatrix(nodes):
+   distMatrix = np.zeros((len(nodes),len(nodes)))
+   for node1 in nodes:
+      for node2 in nodes:
+         distMatrix[node1.id, node2.id] = CalculateDistance(node1, node2)
+   return distMatrix
+
+def CalculateSolutionDistance(nodes):
+    dist = 0
+    for i in range(len(nodes)-1):
+        dist += CalculateDistance(nodes[i],nodes[i+1])
+    return dist
--- tsp/nearest_neighbor.py
+++ tsp/nearest_neighbor.py
@@ -1,52 +1,32 @@
-import random
-import math
-import numpy as np
-from generate_graph import CreateNodes, PlotGraph, Node
+from generate_graph import CreateNodes, PlotGraph, Node, SelectStartNode, CalculateDistMatrix, CalculateSolutionDistance
 import time
 
-
-def SelectStartNode(nodes):
-   random.seed(1)
-   start = random.randint(0,len(nodes)-1)
-   return nodes[start]
-
-def CalculateDistance(node1, node2):
-   return math.sqrt((node1.x - node2.x)**2 + (node1.y - node2.y)**2)
-
-def CalculateDistMatrix(nodes):
-   distMatrix = np.zeros((len(nodes),len(nodes)))
-   for node1 in nodes:
-      for node2 in nodes:
-         distMatrix[node1.id, node2.id] = CalculateDistance(node1, node2)
-   return distMatrix
-
-def SelectClosestNeighbor(distMatrix, targetNode, sortedNodes):
+def SelectClosestNeighbor(distMatrix, targetNode, nodes):
    neighbors = distMatrix[targetNode.id]
    min = 1000000
    minIndex = -1
-   nodeIds = [node.id for node in sortedNodes]
    for i,dist in enumerate(neighbors):
-      if (dist < min and dist != 0 and i not in nodeIds):
+      if (dist < min and dist != 0 and not nodes[i].isVisited):
          min = dist
          minIndex = i
    return minIndex
 
-nodes = CreateNodes(20)
+nodes = CreateNodes(100)
 sortedNodes = []
-tempNodes = nodes[:]
 distMatrix = CalculateDistMatrix(nodes)
 startNode = SelectStartNode(nodes)
-tempNodes.remove(startNode)
 sortedNodes.append(startNode)
 startTime = time.time()
-nextNode = nodes[SelectClosestNeighbor(distMatrix, startNode, sortedNodes)]
-while len(tempNodes) > 0:
-   tempNodes.remove(nextNode)
+nextNode = nodes[SelectClosestNeighbor(distMatrix, startNode, nodes)]
+while len(sortedNodes) < len(nodes):
    sortedNodes.append(nextNode)
-   nextNode = nodes[SelectClosestNeighbor(distMatrix, nextNode, sortedNodes)]
+   nextNode = nodes[SelectClosestNeighbor(distMatrix, nextNode, nodes)]
+   nextNode.isVisited = True
+sortedNodes.append(startNode)
 
 endTime = time.time()
 print('Nearest neighbor time elapsed: ', endTime-startTime)
+print('Nearest neighbor solution distance: ', CalculateSolutionDistance(sortedNodes))
 sortedNodes.append(startNode)
 PlotGraph(sortedNodes)
 
